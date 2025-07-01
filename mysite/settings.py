35844ec8950e88eb017f440c68f6412300bb7b8e
Olimboy <olimboyqazoqov@gmail.com>
2025-07-01 10:36:07 +0500

    Add templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -110,7 +110,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
